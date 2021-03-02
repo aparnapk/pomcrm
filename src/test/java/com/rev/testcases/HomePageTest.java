653489164a2cd63a2adfd9d51fad5e66502fd82c
aparnapk <aparnagopinathpk@gmail.com>
2021-03-02 12:19:01 +1100

    updated homepagetest

--- src/test/java/com/rev/testcases/HomePageTest.java
+++ src/test/java/com/rev/testcases/HomePageTest.java
@@ -21,7 +21,7 @@ public class HomePageTest extends Base {
 		super();
 	}
 
-//this method runs before each test 
+//this method runs before each test in local
 	@BeforeMethod
 	public void setUp() {
 		initialization();
